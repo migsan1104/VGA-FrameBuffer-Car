f2a3684c4943665272333a6b73c4a28359a6754b
migsan1104 <m1104s2003@gmail.com>
2025-08-27 21:12:02 -0400

    Completed clock gen module that included lock to enable safe clock division

---
+++ src/FrameSwap_ctr.sv
@@ -0,0 +1,32 @@
+
+
+module FrameSwapCtrl 
+   (
+  input  logic clk_sys,         // this module exists in the system domain
+  input  logic srst,            // active-high, clk_sys domain
+
+  input  logic swap,   // 1-cycle pulse 
+  input  logic render_idle,     // Renderer reports idle, we can now right
+
+  output logic front_sel_sys,   // sent to frame swap combinational
+  output logic did_swap,        // 1-cycle pulse when swap happens
+  output logic start_render     // 1-cycle pulse that tells the renderer to change states and begin writting
+);
+  always_ff @(posedge clk_sys or posedge srst) begin
+    if (srst) begin
+      front_sel_sys <= 0;
+      did_swap      <= 1'b0;
+      start_render  <= 1'b0;
+    end else begin
+      did_swap     <= 1'b0;
+      start_render <= 1'b0;
+
+      // Swap only at VBLANK start, and only if renderer is idle.
+      if (swap && render_idle) begin
+        front_sel_sys <= ~front_sel_sys;
+        did_swap      <= 1'b1;
+        start_render  <= 1'b1;   // kick renderer to fill the new back buffer
+      end
+    end
+  end
+endmodule
